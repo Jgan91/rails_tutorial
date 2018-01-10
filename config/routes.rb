1b4e30ae17cc68fa40d019392a12c1e6267cdc39
JustinGan <jgan91@gmail.com>
2018-01-10 22:02:07 +1100

    finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root :to => 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  root :to => 'application#hello'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
